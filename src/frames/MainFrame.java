42bd06eec5a726909ba2fe893c46d648a8aa468f
bagadi-alnour <bagadi.alnour@gmail.com>
2021-01-04 14:59:20 +0100

    add menu with items

--- src/frames/MainFrame.java
+++ src/frames/MainFrame.java
@@ -1,5 +1,7 @@
 package frames;
 
+import menu.Menu;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -9,16 +11,20 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class MainFrame extends JFrame implements KeyListener {
+    Menu menu;
     private JPanel avionPanel;
     private final int WIDTH = 400;
     private final int HEIGHT = 800;
 
+
     public MainFrame() throws IOException {
+        menu = new Menu();
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setSize(WIDTH, HEIGHT);
         setResizable(false);
         setLocationRelativeTo(null);
         addKeyListener(this);
+
 
         setTitle("Main frame");
         try {
@@ -38,7 +44,10 @@ public class MainFrame extends JFrame implements KeyListener {
 
         avionPanel.setBounds(90, 90, 110, 110);
         getContentPane().add(avionPanel);
+        setJMenuBar(this.menu.getjMenuBar());
+
         setVisible(true);
+
 
 
     }
